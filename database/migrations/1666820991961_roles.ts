26191a82ba5a48fa5c5e2348eb26ce5234520969
Tom Gobich <tom@tomgobich.com>
2022-10-29 20:31:29 -0400

    switching role record creation to using migration defer

--- database/migrations/1666820991961_roles.ts
+++ database/migrations/1666820991961_roles.ts
@@ -1,4 +1,5 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import Roles from 'App/Enums/Roles'
 
 export default class extends BaseSchema {
   protected tableName = 'roles'
@@ -13,6 +14,14 @@ export default class extends BaseSchema {
        */
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
+    })
+
+    // defer the execution of the callback until after migrations have run
+    this.defer(async (db) => {
+      await db.table('roles').insert([
+        { id: Roles.MEMBER, name: 'Member' },
+        { id: Roles.ADMIN, name: 'Admin' }
+      ])
     })
   }
 
