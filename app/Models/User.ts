ea70e4925a1f5ced4c78d4b8af66342443d5c97b
Tom Gobich <tom@tomgobich.com>
2022-12-21 16:18:20 -0500

    Lesson 5.3 - What Is A Model?

---
+++ app/Models/User.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public roleId: number
+
+  @column()
+  public email: string
+
+  @column()
+  public username: string
+
+  @column()
+  public password: string
+
+  @column()
+  public rememberMeToken: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
